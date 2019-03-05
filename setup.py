dc1a770fd190e5f3aa30046232962ab9540b1dbe
Grey Christoforo <grey@christoforo.net>
2019-03-05 16:59:24 +0100

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mutovis-control",
-    version="1.0.5",
+    version="1.0.6",
     author="Grey Christoforo",
     author_email="grey@mutovis.com",
     description="Software for collecting electrical characterization data for solar cells",
