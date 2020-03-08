a3335fc75bb4c6341f49547131d2272e5271a349
Grey Christoforo <grey@christoforo.net>
2020-03-08 12:58:38 +0000

    setup now does usb gpib stuff

--- setup.sh
+++ setup.sh
@@ -2,3 +2,8 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd "${SCRIPTPATH}"
 ./get-deps.sh
+
+# this stuff is needed only for usb gpib under linux
+sudo modprobe ni_usb
+sudo cp "${SCRIPTPATH}/config/gpib.conf /usr/etc/."
+sudo gpib_config
