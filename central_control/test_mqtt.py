50aa5e7264d9508903b3cbc706be8c35866e8336
jmball <jmball@users.noreply.github.com>
2020-07-31 11:00:21 +0100

    consolidated yaml

--- central_control/test_mqtt.py
+++ central_control/test_mqtt.py
@@ -1,8 +1,8 @@
+"""Test components and functions of the mqtt server."""
+
 import paho.mqtt.client as mqtt
 import pickle
 import time
-
-import yaml
 
 timestamp = time.time()
 
@@ -112,26 +112,6 @@ test_args = {
 }
 test_args["dummy"] = True
 
-
-def yaml_include(loader, node):
-    """Load tagged yaml files into root file."""
-    with open(node.value) as f:
-        return yaml.load(f, Loader=yaml.FullLoader)
-
-
-# bind include function to !include tags in yaml config file
-yaml.add_constructor("!include", yaml_include)
-
-
-def load_config_from_file():
-    """Load the configuration file into memory."""
-    # try to load the configuration file from the current working directory
-    with open("example_config.yaml", "r") as f:
-        config = yaml.load(f, Loader=yaml.FullLoader)
-
-    return config
-
-
 # config = load_config_from_file()
 test_config = {
     "controller": {"uri": "127.0.0.1"},
@@ -233,216 +213,6 @@ test_config = {
 }
 
 
-def test_saver():
-    """Test the saver client."""
-    run_payload = {"args": test_args, "config": test_config}
-
-    raw_ivt_data = [1, 1, 1, 1]
-    raw_ivt_payload = {
-        "data": raw_ivt_data,
-        "idn": "test",
-        "clear": False,
-        "end": False,
-        "sweep": "light",
-        "pixel": {"area": 1},
-    }
-
-    raw_iv_data = [[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3]]
-    raw_iv_payload = {
-        "data": raw_iv_data,
-        "idn": "test",
-        "clear": False,
-        "end": False,
-        "sweep": "light",
-        "pixel": {"area": 1},
-    }
-
-    raw_eqe_data = [1] * 14
-    raw_eqe_payload = {
-        "data": raw_eqe_data,
-        "idn": "test",
-        "clear": False,
-        "end": False,
-        "sweep": "",
-        "pixel": {"area": 1},
-    }
-
-    processed_ivt_data = [1, 1, 1, 1, 2, 2]
-    processed_ivt_payload = {
-        "data": processed_ivt_data,
-        "idn": "test",
-        "clear": False,
-        "end": False,
-        "sweep": "light",
-        "pixel": {"area": 1},
-    }
-
-    processed_iv_data = [[1, 1, 1, 1, 2, 2], [1, 1, 1, 1, 2, 2], [1, 1, 1, 1, 2, 2]]
-    processed_iv_payload = {
-        "data": processed_iv_data,
-        "idn": "test",
-        "clear": False,
-        "end": False,
-        "sweep": "light",
-        "pixel": {"area": 1},
-    }
-
-    processed_eqe_data = [1] * 15
-    processed_eqe_payload = {
-        "data": processed_eqe_data,
-        "idn": "test",
-        "clear": False,
-        "end": False,
-        "sweep": "",
-        "pixel": {"area": 1},
-    }
-
-    cal_eqe_data = [raw_eqe_data, raw_eqe_data, raw_eqe_data]
-    cal_eqe_payload = {"data": cal_eqe_data, "diode": "test", "timestamp": timestamp}
-
-    cal_spectrum_data = [[1, 1], [1, 1], [1, 1]]
-    cal_spectrum_payload = {
-        "data": cal_spectrum_data,
-        "timestamp": timestamp,
-    }
-
-    cal_solarsim_diode_data = [raw_ivt_data, raw_ivt_data, raw_ivt_data]
-    cal_solarsim_diode_payload = {
-        "data": cal_solarsim_diode_data,
-        "diode": "test",
-        "timestamp": timestamp,
-    }
-
-    cal_rtd_data = cal_solarsim_diode_data
-    cal_rtd_payload = {
-        "data": cal_rtd_data,
-        "diode": "test",
-        "timestamp": timestamp,
-    }
-
-    cal_psu_data = [[1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1]]
-    cal_psu_payload = {
-        "data": cal_psu_data,
-        "diode": "test",
-        "timestamp": timestamp,
-    }
-
-    mqttc.publish("measurement/run", pickle.dumps(run_payload), 2).wait_for_publish()
-
-    mqttc.publish(
-        "data/raw/vt_measurement", pickle.dumps(raw_ivt_payload), 2
-    ).wait_for_publish()
-    mqttc.publish(
-        "data/raw/it_measurement", pickle.dumps(raw_ivt_payload), 2
-    ).wait_for_publish()
-    mqttc.publish(
-        "data/raw/mppt_measurement", pickle.dumps(raw_ivt_payload), 2
-    ).wait_for_publish()
-    mqttc.publish(
-        "data/raw/iv_measurement", pickle.dumps(raw_iv_payload), 2
-    ).wait_for_publish()
-    mqttc.publish(
-        "data/raw/eqe_measurement", pickle.dumps(raw_eqe_payload), 2
-    ).wait_for_publish()
-
-    mqttc.publish(
-        "data/processed/vt_measurement", pickle.dumps(processed_ivt_payload), 2
-    ).wait_for_publish()
-    mqttc.publish(
-        "data/processed/it_measurement", pickle.dumps(processed_ivt_payload), 2
-    ).wait_for_publish()
-    mqttc.publish(
-        "data/processed/mppt_measurement", pickle.dumps(processed_ivt_payload), 2
-    ).wait_for_publish()
-    mqttc.publish(
-        "data/processed/iv_measurement", pickle.dumps(processed_iv_payload), 2
-    ).wait_for_publish()
-    mqttc.publish(
-        "data/processed/eqe_measurement", pickle.dumps(processed_eqe_payload), 2
-    ).wait_for_publish()
-
-    mqttc.publish(
-        "calibration/eqe", pickle.dumps(cal_eqe_payload), 2
-    ).wait_for_publish()
-    mqttc.publish(
-        "calibration/spectrum", pickle.dumps(cal_spectrum_payload), 2
-    ).wait_for_publish()
-    mqttc.publish(
-        "calibration/solarsim_diode", pickle.dumps(cal_solarsim_diode_payload), 2
-    ).wait_for_publish()
-    mqttc.publish(
-        "calibration/rtd", pickle.dumps(cal_rtd_payload), 2
-    ).wait_for_publish()
-    mqttc.publish(
-        "calibration/psu/ch1", pickle.dumps(cal_psu_payload), 2
-    ).wait_for_publish()
-    mqttc.publish(
-        "calibration/psu/ch2", pickle.dumps(cal_psu_payload), 2
-    ).wait_for_publish()
-    mqttc.publish(
-        "calibration/psu/ch3", pickle.dumps(cal_psu_payload), 2
-    ).wait_for_publish()
-
-
-def test_analyser():
-    run_payload = {"args": test_args, "config": test_config}
-
-    raw_ivt_data = [1, 1, 1, 1]
-    raw_ivt_payload = {
-        "data": raw_ivt_data,
-        "idn": "test",
-        "clear": False,
-        "end": False,
-        "sweep": "light",
-        "pixel": {"area": 1},
-    }
-
-    raw_iv_data = [[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3]]
-    raw_iv_payload = {
-        "data": raw_iv_data,
-        "idn": "test",
-        "clear": False,
-        "end": False,
-        "sweep": "light",
-        "pixel": {"area": 1},
-    }
-
-    raw_eqe_data = [1] * 14
-    raw_eqe_payload = {
-        "data": raw_eqe_data,
-        "idn": "test",
-        "clear": False,
-        "end": False,
-        "sweep": "",
-        "pixel": {"area": 1},
-    }
-
-    cal_eqe_data = [[0 for i in raw_eqe_data], raw_eqe_data, [2 for i in raw_eqe_data]]
-    cal_eqe_payload = {"data": cal_eqe_data, "diode": "test", "timestamp": timestamp}
-
-    mqttc.publish(
-        "calibration/eqe", pickle.dumps(cal_eqe_payload), 2
-    ).wait_for_publish()
-
-    mqttc.publish("measurement/run", pickle.dumps(run_payload), 2).wait_for_publish()
-
-    mqttc.publish(
-        "data/raw/vt_measurement", pickle.dumps(raw_ivt_payload), 2
-    ).wait_for_publish()
-    mqttc.publish(
-        "data/raw/it_measurement", pickle.dumps(raw_ivt_payload), 2
-    ).wait_for_publish()
-    mqttc.publish(
-        "data/raw/mppt_measurement", pickle.dumps(raw_ivt_payload), 2
-    ).wait_for_publish()
-    mqttc.publish(
-        "data/raw/iv_measurement", pickle.dumps(raw_iv_payload), 2
-    ).wait_for_publish()
-    mqttc.publish(
-        "data/raw/eqe_measurement", pickle.dumps(raw_eqe_payload), 2
-    ).wait_for_publish()
-
-
 def test_run():
     mqttc.publish(
         "measurement/run", pickle.dumps({"args": test_args, "config": test_config}), 2
@@ -533,10 +303,6 @@ if __name__ == "__main__":
     mqttc = mqtt.Client()
     mqttc.connect("127.0.0.1")
     mqttc.loop_start()
-
-    # test_saver()
-
-    # test_analyser()
 
     # test_cal_eqe()
 
