a0c14e178251f196b8e2b29ac1e2cf6193e6a0fd
Greyson Christoforo <grey@christoforo.net>
2020-08-10 16:23:19 +0100

    sync the drivers

--- central_control_dev/illumination.py
+++ central_control_dev/illumination.py
@@ -1,85 +1,74 @@
-from central_control_dev.wavelabs import wavelabs
-
-# from central_control_dev.newport import Newport
+from central_control.wavelabs import wavelabs
+#from central_control.newport import Newport
 import os
 
-
 class illumination:
-    """
+  """
   generic class for handling a light source
   only supports wavelabs and newport via USB (ftdi driver)
   """
+  light_engine = None
 
-    light_engine = None
-
-    def __init__(self, address="", default_recipe="am1_5_1_sun", connection_timeout=10):
-        """
+  def __init__(self, address='', default_recipe='am1_5_1_sun', connection_timeout = 10):
+    """
     sets up communication to light source
     """
 
-        connection_timeout = connection_timeout  # s
+    connection_timeout = connection_timeout # s
 
-        addr_split = address.split(sep="://", maxsplit=1)
-        protocol = addr_split[0]
-        if protocol.lower() == "env":
-            env_var = addr_split[1]
-            if env_var in os.environ:
-                address = os.environ.get(env_var)
-            else:
-                raise ValueError(
-                    "Environment Variable {:} could not be found".format(env_var)
-                )
-            addr_split = address.split(sep="://", maxsplit=1)
-            protocol = addr_split[0]
+    addr_split = address.split(sep='://', maxsplit=1)
+    protocol = addr_split[0]
+    if protocol.lower() == 'env':
+      env_var = addr_split[1]
+      if env_var in os.environ:
+        address = os.environ.get(env_var)
+      else:
+        raise ValueError("Environment Variable {:} could not be found".format(env_var))
+      addr_split = address.split(sep='://', maxsplit=1)
+      protocol = addr_split[0]
 
-        if protocol.lower().startswith("wavelabs"):
-            location = addr_split[1]
-            ls = location.split(":")
-            host = ls[0]
-            if len(ls) == 1:
-                port = None
-            else:
-                port = int(ls[1])
-            if "relay" in protocol.lower():
-                relay = True
-            else:
-                relay = False
-            self.light_engine = wavelabs(
-                host=host,
-                port=port,
-                relay=relay,
-                connection_timeout=connection_timeout,
-                default_recipe=default_recipe,
-            )
-        # elif protocol.lower() == ('ftdi'):
-        #  self.light_engine = Newport(address=address)
+    if protocol.lower().startswith('wavelabs'):
+      location = addr_split[1]
+      ls = location.split(':')
+      host = ls[0]
+      if len(ls) == 1:
+        port = None
+      else:
+        port = int(ls[1])
+      if 'relay' in protocol.lower():
+        relay = True
+      else:
+        relay = False
+      self.light_engine = wavelabs(host=host, port=port, relay=relay, connection_timeout=connection_timeout, default_recipe=default_recipe)
+    #elif protocol.lower() == ('ftdi'):
+    #  self.light_engine = Newport(address=address)
 
-    def connect(self):
-        """
+  def connect(self):
+    """
     makes connection to light source
     """
-        return self.light_engine.connect()
+    return self.light_engine.connect()
 
-    def on(self):
-        """
+  def on(self):
+    """
     turns light on
     """
-        self.light_engine.on()
+    self.light_engine.on()
 
-    def off(self):
-        """
+  def off(self):
+    """
     turns light off
     """
-        self.light_engine.off()
-
-    def get_spectrum(self):
-        """
+    self.light_engine.off()
+  
+  def get_spectrum(self):
+    """
     fetches a spectrum if the light engine supports it
     """
-        return self.light_engine.get_spectrum()
+    self.light_engine.get_spectrum()
 
-    def disconnect(self):
-        """
-        fetches a spectrum if the light engine supports it
-        """
-        self.light_engine.__del__()
+  def disconnect(self):
+    """
+    fetches a spectrum if the light engine supports it
+    """
+    self.light_engine.__del__()
--- central_control_dev/motion.py
+++ central_control_dev/motion.py
@@ -1,98 +1,88 @@
-from central_control_dev.afms import afms
-from central_control_dev.us import us
+from central_control.afms import afms
+from central_control.us import us
 import json
 
-
 class motion:
-    """
+  """
   generic class for handling substrate movement
   """
+  motion_engine = None
 
-    motion_engine = None
+  # these should be overwritten by a motion controller implementation
+  #substrate_centers = [160, 140, 120, 100, 80, 60, 40, 20]  # mm from home to the centers of A, B, C, D, E, F, G, H substrates
+  #photodiode_location = 180  # mm
 
-    # these should be overwritten by a motion controller implementation
-    # substrate_centers = [160, 140, 120, 100, 80, 60, 40, 20]  # mm from home to the centers of A, B, C, D, E, F, G, H substrates
-    # photodiode_location = 180  # mm
-
-    def __init__(self, address="", pcb_object=None):
-        """
+  def __init__(self, address='', pcb_object = None):
+    """
     sets up communication to motion controller
     """
-        if address.startswith("afms://"):  # adafruit motor shield
-            self.motion_engine = afms(address=address)
-            self.substrate_centers = self.motion_engine.substrate_centers
-            self.photodiode_location = self.motion_engine.photodiode_location
-        elif address.startswith(
-            "us://"
-        ):  # uStepperS via i2c via ethernet connected pcb
-            content = address.lstrip("us://")
-            pieces = content.split("/")
-            expected_lengths_in_mm = pieces[0]
-            steps_per_mm = float(pieces[1])
+    if address.startswith('afms://'):  # adafruit motor shield
+      self.motion_engine = afms(address=address)
+      self.substrate_centers = self.motion_engine.substrate_centers
+      self.photodiode_location = self.motion_engine.photodiode_location
+    elif address.startswith('us://'):  # uStepperS via i2c via ethernet connected pcb
+      content = address.lstrip('us://')
+      pieces = content.split('/')
+      expected_lengths_in_mm = pieces[0]
+      steps_per_mm = float(pieces[1])
 
-            expected_lengths_in_mm = expected_lengths_in_mm.split(",")
-            expected_lengths_in_mm = [float(x) for x in expected_lengths_in_mm]
-            steps_per_mm = round(steps_per_mm)
+      expected_lengths_in_mm = expected_lengths_in_mm.split(',')
+      expected_lengths_in_mm = [float(x) for x in expected_lengths_in_mm]
+      steps_per_mm = round(steps_per_mm)
 
-            extra = ""
-            keepout = [[]] * len(expected_lengths_in_mm)
-            if len(pieces) >= 3:
-                try:
-                    keepout = json.loads(pieces[2])
-                except:
-                    keepout = [[]] * len(expected_lengths_in_mm)
-                    extra = pieces[2]
-                if len(pieces) == 4:
-                    extra = pieces[3]
+      extra = ''
+      keepout = [[]]*len(expected_lengths_in_mm)
+      if len(pieces) >= 3:
+        try:
+          keepout = json.loads(pieces[2])
+        except:
+          keepout = [[]]*len(expected_lengths_in_mm)
+          extra = pieces[2]
+        if len(pieces) == 4:
+          extra = pieces[3]
+      
+      # so the format is now
+      # driver://csv list of expected lengths in mm/steps per mm/json formatted list of lists of max and min keepout zones/extra
+      # for example:
+      # us://875,375/6400/[[],[]]
 
-            # so the format is now
-            # driver://csv list of expected lengths in mm/steps per mm/json formatted list of lists of max and min keepout zones/extra
-            # for example:
-            # us://875,375/6400/[[],[]]
+      self.motion_engine = us(pcb_object, expected_lengths=expected_lengths_in_mm, keepout_zones=keepout, steps_per_mm=steps_per_mm, extra=extra)
+      #self.substrate_centers = self.motion_engine.substrate_centers
+      #self.photodiode_location = self.motion_engine.photodiode_location
 
-            self.motion_engine = us(
-                pcb_object,
-                expected_lengths=expected_lengths_in_mm,
-                keepout_zones=keepout,
-                steps_per_mm=steps_per_mm,
-                extra=extra,
-            )
-            # self.substrate_centers = self.motion_engine.substrate_centers
-            # self.photodiode_location = self.motion_engine.photodiode_location
 
-    def connect(self):
-        """
+  def connect(self):
+    """
     makes connection to motion controller, blocking
     """
-        return self.motion_engine.connect()
+    return self.motion_engine.connect()
 
-    def move(self, mm):
-        """
+  def move(self, mm):
+    """
     moves mm mm direction, blocking, returns 0 on successful movement
     """
-        return self.motion_engine.move(mm)
+    return self.motion_engine.move(mm)
 
-    def goto(self, pos):
-        """
+  def goto(self, pos):
+    """
     goes to an absolute mm position, blocking, reuturns 0 on success
     """
-        return self.motion_engine.goto(pos)
+    return self.motion_engine.goto(pos)
 
-    def home(self):
-        """
+  def home(self):
+    """
     homes to a limit switch, blocking, reuturns 0 on success
     """
-        return self.motion_engine.home()
+    return self.motion_engine.home()
 
-    def estop(self):
-        """
+  def estop(self):
+    """
     emergency stop of the driver
     """
-        return self.motion_engine.estop()
+    return self.motion_engine.estop()
 
-    def get_position(self):
-        """
+  def get_position(self):
+    """
     returns the current stage location in mm
     """
-        return self.motion_engine.get_position()
-
+    return self.motion_engine.get_position()
