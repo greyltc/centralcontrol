bfb4bee331277bfa0821ad6b141f663e467bc2ca
Greyson Christoforo <grey@christoforo.net>
2020-09-30 13:21:45 +0100

    remove some refs to dev

--- util/bash_run_the_services.sh
+++ util/bash_run_the_services.sh
@@ -14,7 +14,7 @@ python -m plotter.eqe_plotter --mqtthost 127.0.0.1 --dashhost 127.0.0.1 &
 python -m plotter.mppt_plotter --mqtthost 127.0.0.1 --dashhost 127.0.0.1 &
 
 python -m central_control.utility_handler --address 127.0.0.1 &
-python -m central_control_dev.mqtt_server --mqtthost 127.0.0.1 &
+python -m central_control.mqtt_server --mqtthost 127.0.0.1 &
 
 mkdir -p /tmp/data
 cd /tmp/data
