33e2112b796341ba2faf01588b3a5769d41aa9e9
Greyson Christoforo <grey@christoforo.net>
2021-06-01 19:34:29 +0100

    add maxstep mppt custom param

--- src/centralcontrol/mppt.py
+++ src/centralcontrol/mppt.py
@@ -138,10 +138,10 @@ class mppt:
         m.append(m_tracked:=self.gradient_descent(duration, start_voltage=self.Vmpp, alpha=5, min_step=0.001, NPLC=10, callback=callback, delay=1000, snaith_mode=True))
       else:
         params = params.split(':')
-        if len(params) != 4:
-          raise (ValueError("MPPT configuration failure, Usage: --mppt-params snaith://[alpha]:[min_step]:[NPLC]:[delayms]"))
+        if len(params) != 5:
+          raise (ValueError("MPPT configuration failure, Usage: --mppt-params snaith://[alpha]:[min_step]:[NPLC]:[delayms]:[max_step]"))
         params = [float(f) for f in params]
-        m.append(m_tracked:=self.gradient_descent(duration, start_voltage=self.Vmpp, callback=callback, alpha=params[0], min_step=params[1], NPLC=params[2]), snaith_mode=True, delay=params[3])
+        m.append(m_tracked:=self.gradient_descent(duration, start_voltage=self.Vmpp, callback=callback, alpha=params[0], min_step=params[1], NPLC=params[2]), snaith_mode=True, delay=params[3], max_step=params[4])
     else:
       print('WARNING: MPPT algorithm {:} not understood, not doing max power point tracking'.format(algo))
 
@@ -150,14 +150,14 @@ class mppt:
     print('{:0.4f} mW @ {:0.2f} mV and {:0.2f} mA'.format(self.Vmpp*self.Impp*1000*-1, self.Vmpp*1000, self.Impp*1000))
     return (m, ssvocs)
 
-  def gradient_descent(self, duration, start_voltage, callback=lambda x:None, alpha=0.1, min_step=0.001, NPLC=-1, snaith_mode=False, delay=1000):
+  def gradient_descent(self, duration, start_voltage, callback=lambda x:None, alpha=0.1, min_step=0.001, NPLC=-1, snaith_mode=False, delay=1000, max_step=0.1):
     """
     gradient descent MPPT algorithm
     alpha is the "learning rate"
     min_step is the minimum voltage step size the algorithm will be allowed to take
     delay is the number of ms to wait between setting the voltage and making a measurement
     """
-    max_step = 0.1
+    max_step = max_step
     snaith_pre_soak_t = 15
     snaith_post_soak_t = 3
     self.delay = delay
