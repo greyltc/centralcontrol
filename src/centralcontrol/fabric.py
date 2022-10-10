700746901bbb0b0a27722c86983f8b3d12ab888c
Greyson Christoforo <grey@christoforo.net>
2022-10-10 17:06:38 +0100

    fix unique check

--- src/centralcontrol/fabric.py
+++ src/centralcontrol/fabric.py
@@ -816,7 +816,7 @@ class Fabric(object):
                     assert pixel["did"] > 0, "Registering device failed"
                     lu["device_ids"].append(pixel["did"])
 
-            assert (lu["device_ids"]) == len(list(set(lu["device_ids"]))), "Every device in a run must be unique."
+            assert len(lu["device_ids"]) == len(list(set(lu["device_ids"]))), "Every device in a run must be unique."
 
             # check connectivity
             self.lg.log(29, f"Checking device connectivity...")
